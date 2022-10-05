1de3efca40614da06a37ec84a973abd05f0c9496
janjan97 <eoen482@nate.com>
2022-10-05 16:26:08 +0900

    Add] 증감 연산자 (increment decrement operators)

---
+++ Chapter3/Chapter3_3/Chapter3_3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+/*
+int add(int a, int b)
+{
+    return a + b;
+}*/
+
+int main()
+{
+    using namespace std;
+
+    
+    int x = 6, y = 6;
+    cout << x << " " << y << endl;
+    cout << ++x << " " << --y << endl;
+    cout << x << " " << y << endl;
+    cout << x++ << " " << y-- << endl;
+    cout << x << " " << y << endl;
+    
+
+    /*int x = 1;
+    int v = add(x, ++x);
+    
+    cout << v << endl;*/
+
+    return 0;
+}

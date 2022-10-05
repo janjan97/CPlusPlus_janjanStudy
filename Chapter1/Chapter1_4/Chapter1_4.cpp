80306feb64ddf80a0f5ce137dbae7819a96b39cc
janjan97 <eoen482@nate.com>
2022-10-05 15:57:13 +0900

    ADD] 입출력 스트림

---
+++ Chapter1/Chapter1_4/Chapter1_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // cout, cin, endl, ...
+#include <cstdio> //printf
+
+int main()
+{
+    using namespace std;
+
+    int x = 1;
+
+    cout << "Before your input, x was " << x << endl;
+
+    cin >> x;
+
+    cout << "your input is " << x << endl;
+
+
+    return 0;
+}

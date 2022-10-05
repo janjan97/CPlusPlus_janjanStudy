63158d53324d0562230fcdcdef42cd9cb119d5ec
janjan97 <eoen482@nate.com>
2022-10-05 16:29:23 +0900

    Add] 비트 플래그, 비트 마스크 사용법_2 (Bit flags, Bit masks)

---
+++ Chapter3/Chapter3_9_2/Chapter3_9_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bitset>
+
+// »ö ÃßÃâ
+
+using namespace std;
+
+int main()
+{
+	const unsigned int red_mask = 0xFF0000;
+	const unsigned int green_mask = 0x00FF00;
+	const unsigned int blue_mask = 0x0000FF;
+
+	cout << std::bitset<32>(red_mask) << endl;
+	cout << std::bitset<32>(green_mask) << endl;
+	cout << std::bitset<32>(blue_mask) << endl;
+
+
+	unsigned int pixel_color = 0xDAA520;
+
+	cout << std::bitset<32>(pixel_color) << endl;
+
+	unsigned char red = (pixel_color & red_mask) >> 16;
+	unsigned char green = (pixel_color & green_mask) >> 8;
+	unsigned char blue = pixel_color & blue_mask;
+
+	cout << "red " << std::bitset<8>(red) << " " << int(red) << endl;
+	cout << "green " << std::bitset<8>(green) << " " << int(green) << endl;
+	cout << "blue " << std::bitset<8>(blue) << " " << int(blue) << endl;
+
+
+	return 0;
+}

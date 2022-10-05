ea67dabeba7ef3bb82f750301f8b118494dc06da
janjan97 <eoen482@nate.com>
2022-10-05 16:14:26 +0900

    Add] 무치형 (Void)

---
+++ Chapter2/Chapter2_4/Chapter2_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void my_function()
+{
+
+}
+int main()
+{
+
+    int i = 123;
+    float f = 123.456f;
+
+    void* my_void;
+
+    my_void = (void*)&i;
+    my_void = (void*)&f;
+
+    return 0;
+}

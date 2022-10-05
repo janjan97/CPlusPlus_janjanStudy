b45a02a6b502e6a33ce2f845c1b95f583eea9107
janjan97 <eoen482@nate.com>
2022-10-05 17:42:04 +0900

    Add] C 스타일의 문자열 심볼릭(기호적) 상수

---
+++ Chapter6/Chapter6_10/Chapter6_10.cpp
@@ -0,0 +1,28 @@
+// C스타일의 문자열 기호적(심볼릭) 상수
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	/*const char* name = "jin jin";
+	const char* name2 = "jin jin";
+
+	cout << (uintptr_t)name << endl;
+	cout << (uintptr_t)name2 << endl;*/
+
+	/*int int_arr[5] = { 1,2,3,4,5 };
+	char char_arr[] = "Hello, Wolrd!";
+	const char* name = "Jin Jin";
+
+	cout << int_arr << endl;
+	cout << char_arr << endl;
+	cout << name << endl;*/
+
+	char c = 'Q';
+	cout << &c << endl;
+
+	
+    return 0;
+}

2e952dc499be0f161ce0e80a8ff2ce213c4aa446
janjan97 <eoen482@nate.com>
2022-10-05 17:00:50 +0900

    Add] 제어 흐름 (Control Flow)

---
+++ Chapter5/Chapter5_1/Chapter5_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	cout << "I Miss U" << endl;
+
+	exit(0); // return을 하지않고 exit(0)로 중단
+
+	/*cout << "I Want be alone " << endl;
+
+	return 0; */
+}

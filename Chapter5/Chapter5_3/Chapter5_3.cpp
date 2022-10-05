db5cc30d6c9bb92303016caa85d03a62c02ca4d8
janjan97 <eoen482@nate.com>
2022-10-05 17:01:36 +0900

    Add] 스위치문 (switch-case)

---
+++ Chapter5/Chapter5_3/Chapter5_3.cpp
@@ -0,0 +1,47 @@
+// switch - case
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+        /*switch (x)
+        {
+            
+        case 0:
+            cout << "Zero";
+            break;
+        case 1:
+            cout << "One";
+            break;
+        case 2:       
+            cout << "Two";
+            break;
+        }*/
+        /*
+        switch (x)
+        {
+            int a;
+            //int b = 1 ; // ¿À·ù 
+          
+        case 0:         
+            cout << a << endl;
+        }
+        */
+
+    {
+        int y = 0;
+
+        switch (x)
+        {
+        case 0:
+            cout << y << endl;
+        }
+    }
+
+        
+    return 0;
+}

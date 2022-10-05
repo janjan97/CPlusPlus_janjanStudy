8ab77890d440dd213307e1b0f7036ddd51c5f700
janjan97 <eoen482@nate.com>
2022-10-05 16:01:44 +0900

    ADD] 선언과 정의

---
+++ Chapter1/Chapter1_10/Chapter1_10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b); //전방선언 (foward declaration)
+int multiply(int a, int b);
+int subtract(int a, int b);
+
+int main()
+{
+    cout << add(1, 2) << endl;
+    cout << subtract(1, 2) << endl;
+
+    return 0;
+}
+
+//definition
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}

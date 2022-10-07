4b6b785a87de391ee642704c6795d683b93f09bd
janjan97 <eoen482@nate.com>
2022-10-07 22:38:29 +0900

    Add] 주소에 의한 인수 전달 (Call by Address)

---
+++ Chapter7/Chapter7_4/Chapter7_4.cpp
@@ -0,0 +1,25 @@
+﻿// 주소에 의한 인수전달 ( Call by Address )
+#include <iostream>
+
+using namespace std;
+
+void foo(int *ptr)
+{
+    cout << *ptr << " " << ptr << " " << &ptr << endl;
+}
+
+int main()
+{
+    int value = 5;
+    int* ptr = &value;
+
+    cout << value << " " << &value << endl;
+    cout << &ptr << endl;
+
+    foo(ptr);
+    foo(&value);
+    //foo(5);
+
+    return 0;
+}
+

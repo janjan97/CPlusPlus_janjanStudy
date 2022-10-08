d6a2ac80c69439d3c48f628bb04688c694ab9996
janjan97 <eoen482@nate.com>
2022-10-08 23:39:34 +0900

    Add] 매개변수의 기본 값

---
+++ Chapter7/Chapter7_8/Chapter7_8.cpp
@@ -0,0 +1,14 @@
+﻿// Chapter7_8.cpp : 매개변수의 기본값
+#include <iostream>
+using namespace std;
+
+void print(std::string str){}
+void print(char ch = ' '){}
+
+int main() 
+{
+    print();
+  
+    return 0;
+}
+

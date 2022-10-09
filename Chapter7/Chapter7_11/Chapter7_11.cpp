43ce79ce1475ab5efe5916a87b0fda9647fc153a
janjan97 <eoen482@nate.com>
2022-10-09 19:38:43 +0900

    Add] vector를 스택처럼 사용하기

---
+++ Chapter7/Chapter7_11/Chapter7_11.cpp
@@ -0,0 +1,40 @@
+﻿// Chapter7_11.cpp : std::vector를 스택처럼 사용하기
+#include <iostream>
+#include <vector>
+using namespace std;
+// size, capaciity(용량)
+
+void printStack(const vector<int>& stack)
+{
+	for (auto& e : stack)
+		cout << e << " ";
+	cout << endl;
+}
+
+int main()
+{
+	vector<int> stack;
+
+	stack.reserve(1024); //메모리 1024만큼 확보, 너무크면 낭비가될 수도 있다.
+
+	stack.push_back(3);
+	printStack(stack);
+
+	stack.push_back(5);
+	printStack(stack);
+
+	stack.push_back(7);
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+	stack.pop_back();
+	printStack(stack);
+
+    return 0;
+}
+

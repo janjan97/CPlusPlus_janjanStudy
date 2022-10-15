87644f9459976a68609bf8ba18f681c92ea17d9d
janjan97 <eoen482@nate.com>
2022-10-15 14:48:42 +0900

    Add] class와 friend 함수

---
+++ Chapter8/Chapter8_12/Chapter8_12.cpp
@@ -0,0 +1,41 @@
+﻿// Chapter8_12.cpp : friend 함수 
+#include <iostream>
+using namespace std;
+
+class A; //전방 선언
+
+class B
+{
+private:
+    int m_value = 2;
+
+public:
+    void doSomething(A& a);
+
+};
+
+class A
+{
+private:
+	int m_value = 1;
+
+	//friend class B;
+	friend void B::doSomething(A& a);
+};
+
+
+void B::doSomething(A& a)
+{
+	cout << a.m_value << endl;
+}
+
+int main()
+{
+    A a;
+    B b;
+
+    b.doSomething(a);
+
+    return 0;
+}
+

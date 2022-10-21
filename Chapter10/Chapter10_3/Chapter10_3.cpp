96caba4c12d60530de69368a144cbb04eb4bb3e2
janjan97 <eoen482@nate.com>
2022-10-21 19:40:14 +0900

    Add] 집합 관계 (Aggregation)

---
+++ Chapter10/Chapter10_3/Chapter10_3.cpp
@@ -0,0 +1,42 @@
+﻿// Chapter10_3.cpp : 집합관계 (Aggregation)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Lecture.h"
+
+int main()
+{
+	using namespace std;
+
+	Student std1("Jan Jan", 0);
+	Student std2("Dash", 1);
+	Student std3("Violet", 2);
+
+	Teacher teacher1("Prof. Hong");
+	Teacher teacher2("Prof. Jeong");
+
+		//composition relationship
+	Lecture lec1("Introduction to computer programming");
+	lec1.assignTeacher(&teacher1);
+	lec1.registerStudent(&std1);
+	lec1.registerStudent(&std2);
+	lec1.registerStudent(&std3);
+
+	Lecture lec2("Computational Thinking");
+	lec2.assignTeacher(&teacher2);
+	lec2.registerStudent(&std1);
+
+	{
+		cout << lec1 << endl;
+		cout << lec2 << endl;
+
+		//event
+		lec2.study();
+
+		cout << lec1 << endl;
+		cout << lec2 << endl;
+	}
+
+	return 0;
+}
---
+++ Chapter10/Chapter10_3/Lecture.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include <vector>
+#include "Student.h"
+#include "Teacher.h"
+
+class Lecture
+{
+private:
+	std::string m_name;
+
+	Teacher* teacher;
+	std::vector<Student*> students; //학생 다수
+
+public:
+	Lecture(const std::string & name_in)
+		: m_name(name_in)
+	{}
+
+	~Lecture()
+	{
+		//do not delete teacher
+		//do not delete students
+	}
+
+	//포인터
+	void assignTeacher(Teacher * const teacher_input) // *, const제거
+	{
+		teacher = teacher_input;
+	}
+
+	void registerStudent(Student * const student_input)
+	{
+		students.push_back(student_input);
+	}
+
+	void study()
+	{
+		std::cout << m_name << "Study " << std::endl << std::endl;
+
+		// value가 아닌 포인터
+		for (auto& element : students)
+			(* element).setIntel((* element).getIntel() + 1); //(* element) *(역참조)
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const Lecture& lecture)
+	{
+		out << "Lecture name: " << lecture.m_name << std::endl;
+
+		out << lecture.teacher << std::endl;
+		for (auto element : lecture.students)
+			out << *element << std::endl; //element는 주소자체를 출력함
+
+		return out;
+	}
+};

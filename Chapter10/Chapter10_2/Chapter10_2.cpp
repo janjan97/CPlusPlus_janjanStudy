fe03f2c22e0f84d9550f09548f090018a3e2975a
janjan97 <eoen482@nate.com>
2022-10-21 19:39:51 +0900

    Add] 구성 관계 (Composition)

---
+++ Chapter10/Chapter10_2/Chapter10_2.cpp
@@ -0,0 +1,20 @@
+﻿// Chapter10_2.cpp : 구성관계 (Composition)
+#include <iostream>
+#include "Monster.h"
+using namespace std;
+
+int main()
+{
+	Monster mon1("janjan", Position2D(0,0));
+	cout << mon1 << endl;
+
+	//while (1) //game loop
+	{
+
+		//event
+		mon1.moveTo(Position2D(1,1));
+		cout << mon1 << endl;
+	}
+
+	return 0;
+}

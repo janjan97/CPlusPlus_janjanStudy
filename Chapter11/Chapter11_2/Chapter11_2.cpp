69b525ffb9a76b99051dad68c8af3c9e8348acf1
janjan97 <eoen482@nate.com>
2022-10-27 23:34:50 +0900

    Add] 상속(Inheritance) [2/2]

---
+++ Chapter11/Chapter11_2/Chapter11_2.cpp
@@ -0,0 +1,31 @@
+﻿// Chapter11_2.cpp : 상속의 기본 [2 / 2]
+#include <iostream>
+#include "Student.h"
+#include "Teacher.h"
+
+int main()
+{
+    Student std("Jan Jan");
+    std.setName("Jan Jan 2");
+    std::cout << std.getName() << std::endl;
+
+    Teacher teacher1("Dr. H");
+    teacher1.setName("Dr. K");
+
+    std::cout << teacher1.getName() << std::endl;
+
+    std::cout << std << std::endl;
+    std::cout << teacher1 << std::endl;
+
+    std.doNothing();
+    teacher1.doNothing();
+
+    std.study();
+    teacher1.teach();
+    
+    Person person;
+    person.setName("Mr. Incredible");
+    person.getName();
+    //자식클래스의 것을 부모클래스가 쓰지못함(study, teach)
+
+}
---
+++ Chapter11/Chapter11_2/Teacher.h
@@ -0,0 +1,29 @@
+#pragma once
+
+//#include <string>
+#include "Person.h"
+
+class Teacher : public Person
+{
+private:
+	//std::string m_name;
+	
+
+public:
+	Teacher(const std::string& name_in = "No name")//»ý¼ºÀÚ
+		: Person(name_in)
+	{}
+
+	void teach()
+	{
+		std::cout << getName() << " is teaching" << std::endl;
+	}
+
+	friend std::ostream& operator <<(std::ostream& out, const Teacher& teacher)
+	{
+		//out << teacher.m_name; <<private
+		out << teacher.getName();
+
+		return out;
+	}
+};

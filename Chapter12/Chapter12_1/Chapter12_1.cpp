a01c9b9fc1193155f33f1ad669009bb4b86f4c1c
janjan97 <eoen482@nate.com>
2022-10-29 23:48:03 +0900

    Add] 다형성의 기본

---
+++ Chapter12/Chapter12_1/Chapter12_1.cpp
@@ -0,0 +1,72 @@
+﻿// Chapter12_1.cpp : 다형성의 기본 개념
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected:
+	string m_name;
+
+public:
+	Animal(std::string name)
+		: m_name(name)
+	{}
+
+public:
+	string getName() { return m_name; }
+
+	virtual void speak() const //virtual
+	{
+		cout << m_name << " ??? " << endl;
+	}
+};
+
+class Cat : public Animal
+{
+public:
+	Cat(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << "Meow " << endl;
+	}
+
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << "Woof " << endl;
+	}
+};
+
+int main()
+{
+	Cat cats[] = { Cat("cat1"), Cat("cat2"), Cat("cat3"), Cat("cat4"), Cat("cat5") };
+	Dog dogs[] = { Dog("dog1"), Dog("dog2") };
+
+	for (int i = 0; i < 5; ++i)
+		cats[i].speak();
+
+	for (int i = 0; i < 2; ++i)
+		dogs[i].speak();
+
+	//...동물들이 추가된다면..?
+	
+	Animal* my_animals[] = { &cats[0], &cats[1], &cats[2], &cats[4],
+	&dogs[0], &dogs[1]};
+
+	for (int i = 0; i < 7; ++i)
+		my_animals[i]->speak();
+
+	return 0;
+}

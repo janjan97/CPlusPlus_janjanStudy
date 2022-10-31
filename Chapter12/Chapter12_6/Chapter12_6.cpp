ac44b3d882231304f0cdd97c5a502334061c843f
janjan97 <eoen482@nate.com>
2022-11-01 00:22:15 +0900

    Add] 가상 함수 표 (Virtual Table)

---
+++ Chapter12/Chapter12_6/Chapter12_6.cpp
@@ -0,0 +1,26 @@
+﻿// Chapter12_6.cpp : 가상 함수 표 (Virtual Tables)
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+	void fun1() {};
+	void fun2() {};
+};
+
+class Derived : public Base
+{
+public:
+	void fun1() {};
+	void fun3() {};
+};
+
+int main()
+{
+	cout << sizeof(Base) << endl;
+	cout << sizeof(Derived) << endl;
+
+	return 0;
+}
+

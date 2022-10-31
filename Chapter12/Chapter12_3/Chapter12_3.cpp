bdaad4e117f942fa7a761c5c5c36fa3d85592306
janjan97 <eoen482@nate.com>
2022-11-01 00:21:34 +0900

    Add] override, final, 공변 반환 값

---
+++ Chapter12/Chapter12_3/Chapter12_3.cpp
@@ -0,0 +1,39 @@
+﻿// Chapter12_3.cpp : override, final, 공변 반환값
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	void print() { cout << "A" << endl; }
+	virtual A* getThis() {
+		cout << "A::getThis()" << endl;
+		return this;
+	}
+};
+
+class B : public A
+{
+public:
+	void print() { cout << "B" << endl; }
+	virtual B* getThis() {
+		cout << "B::getThis()" << endl;
+		return this;
+	}
+
+};
+
+int main()
+{
+	A a;
+	B b;
+
+	A& ref = b;
+	b.getThis()->print();
+	ref.getThis()->print();
+
+	cout << typeid(b.getThis()).name() << endl;
+	cout << typeid(ref.getThis()).name() << endl;
+
+	return 0;
+}

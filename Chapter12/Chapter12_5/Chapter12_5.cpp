4ddfa1d8dc323b0e629c198b432b11e1868ffc8c
janjan97 <eoen482@nate.com>
2022-11-01 00:21:58 +0900

    Add] 정적 바인딩, 동적 바인딩

---
+++ Chapter12/Chapter12_5/Chapter12_5.cpp
@@ -0,0 +1,45 @@
+﻿// Chapter12_5.cpp : 동적바인딩, 정적바인딩
+#include <iostream>
+using namespace std;
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int subtract(int x, int y)
+{
+	return x - y;
+}
+
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+
+	int op;
+
+	cout << "0: add, 1: subtract" << endl;
+	cin >> op;
+
+	//static binding (early binding)
+	/*
+	int result;
+	switch (op)
+	{
+	case 0: result = add(x, y); break;
+	case 1: result = subtract(x, y); break;
+	}
+
+	cout << result << endl;
+	*/
+
+	// Dynamic binding (late binding)
+	int(*func_ptr)(int, int) = nullptr;
+	switch (op)
+	{
+	case 0: func_ptr = add; break;
+	case 1: func_ptr = subtract; break;
+	}
+
+}

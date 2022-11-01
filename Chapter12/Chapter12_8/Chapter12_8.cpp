e6db6bf69e1d1f84a4c2649823af2b3e3d880f5b
janjan97 <eoen482@nate.com>
2022-11-01 23:54:11 +0900

    Add] 가상 기본 클래스와 다이아몬드 상속 문제 해결

---
+++ Chapter12/Chapter12_8/Chapter12_8.cpp
@@ -0,0 +1,55 @@
+﻿// Chapter12_8.cpp : 가상 기본 클래스와 다이아몬드 상속 문제
+#include <iostream>
+using namespace std;
+
+class PoweredDevice
+{
+public:
+	int m_i;
+
+	PoweredDevice(int power)
+	{
+		cout << "PoweredDevice: " << power << "\n";
+	}
+};
+
+class Scanner : virtual public PoweredDevice
+{
+public:
+	Scanner(int scanner, int power)
+		:PoweredDevice(power)
+	{
+		cout << "Scanner: " << scanner << "\n";
+	}
+};
+
+class Printer : virtual public PoweredDevice
+{
+public:
+	Printer(int printer, int power)
+		: PoweredDevice(power)
+	{
+		cout << "Printer: " << printer << "\n";
+	}
+};
+
+class Copier : public Scanner, public Printer
+{
+public:
+	Copier(int scanner, int printer, int power)
+		: Scanner(scanner, power), Printer(printer, power),
+		PoweredDevice(power)
+	{}
+
+};
+
+int main()
+{
+	Copier cop(1, 2, 3); //생성자 호출
+
+	cout << &cop.Scanner::PoweredDevice::m_i << endl;
+	cout << &cop.Printer::PoweredDevice::m_i << endl;
+
+	return 0;
+}
+

37f8d60f732ba626b5b2863289d3bcbd7dd13b8b
janjan97 <eoen482@nate.com>
2022-11-01 23:53:48 +0900

    Add] 순수 가상 함수, 추상 클래스, 인터페이스

---
+++ Chapter12/Chapter12_7/Chapter12_7.cpp
@@ -0,0 +1,64 @@
+﻿// Chapter12_7.cpp : 순수 가상 함수, 추상 클래스, 인터페이스
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected:
+	string m_name;
+public:
+	Animal(std::string name)
+		:m_name(name)
+	{}
+public:
+	string getName() { return m_name; }
+	virtual void speak() const = 0;  //순수 가상 함수
+};
+
+void Animal::speak() const
+{
+	cout << m_name << "???" << endl;
+}
+
+class Cat : public Animal
+{
+protected:
+	string m_name;
+public:
+	Cat(std::string name)
+		:Animal(name)
+	{}
+
+	virtual void speak() const
+	{
+		cout << m_name << "Mao" << endl;
+	}
+};
+
+/*
+//인터페이스 클래스
+class IErrorLog
+{
+public:
+	virtual bool reportError(const char* error) = 0;
+	virtual ~IErrorLog() {}
+};
+
+class FileErrorLg : public IErrorLog 
+{
+public:
+	bool reportError(const char* errorMessage) override
+	{
+		cout << "Writing Error To a File" << endl;
+		return true;
+	}
+};
+*/
+
+int main()
+{
+	Cat cat("hello");
+	
+}
+

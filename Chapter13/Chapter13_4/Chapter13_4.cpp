5711de02bac7b6719b09850a5bc3a9d30337d798
janjan97 <eoen482@nate.com>
2022-11-03 19:18:22 +0900

    Add] 함수 템플릿 특수화 (template<>)

---
+++ Chapter13/Chapter13_4/Chapter13_4.cpp
@@ -0,0 +1,28 @@
+﻿// Chapter13_4.cpp : 함수 템플릿 특수화
+#include <iostream>
+#include "Storage.h"
+using namespace std;
+
+template<typename T>
+T getMax(T x, T y)
+{
+	return (x > y) ? x : y;
+}
+
+template<>
+char getMax(char x, char y)
+{
+	cout << "warning: comparing chars" << endl;
+	return (x > y) ? x : y;
+}
+
+int main()
+{
+	Storage<int> nValue(5);
+	Storage<double> dValue(6.7);
+
+	nValue.print();
+	dValue.print();
+
+	return 0;
+}

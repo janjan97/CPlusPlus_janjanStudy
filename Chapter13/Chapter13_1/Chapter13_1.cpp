65bceb41acef959a600b5111df43e97d3b61e769
janjan97 <eoen482@nate.com>
2022-11-03 19:14:07 +0900

    Add] 함수 템플릿 (template<typename T>)

---
+++ Chapter13/Chapter13_1/Chapter13_1.cpp
@@ -0,0 +1,20 @@
+﻿// Chapter13_1.cpp : 함수 템플릿 (Templates)
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T getMax(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+
+int main()
+{
+    cout << getMax(1, 2) << endl;
+    cout << getMax(3.14, 1.592) << endl;
+    cout << getMax(1.0f, 3.4f) << endl;
+    cout << getMax('a', 'c') << endl;
+
+    return 0;
+}

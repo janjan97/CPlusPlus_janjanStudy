807964a7a467f4c9b6e3cf1d0091f6f202580820
janjan97 <eoen482@nate.com>
2022-11-03 19:21:11 +0900

    Add] 포인터에 대한 템플릿 특수화

---
+++ Chapter13/Chapter13_7/Chapter13_7.cpp
@@ -0,0 +1,58 @@
+﻿// Chapter13_7.cpp : 포인터에 대한 템플릿 특수화
+#include <iostream>
+using namespace std;
+
+template<class T>
+class A
+{
+private:
+	T m_value;
+
+public:
+	A(const T& input) 
+		: m_value(input)
+	{
+
+	}
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
+
+template<class T>
+class A<T*> //<T*> T가 포인터일때만
+{
+private:
+	T* m_value; // T*
+
+public:
+	A(T* input) // T*
+		: m_value(input)
+	{
+
+	}
+
+	void print()
+	{
+		cout << *m_value << endl; //*
+	}
+};
+
+int main()
+{
+	A<int> a_int(123);
+	a_int.print();
+
+	int temp = 456;
+
+	A<int*> a_int_ptr(&temp);  //int*
+	a_int_ptr.print();
+
+	double temp_d = 3.141592;
+	A<double*> a_double_ptr(&temp_d);  //double*
+	a_double_ptr.print();
+
+	return 0;
+}

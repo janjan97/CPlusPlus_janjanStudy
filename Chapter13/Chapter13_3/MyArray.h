1b07059be8ee67fe26310ff171af37a78d3dae97
janjan97 <eoen482@nate.com>
2022-11-03 19:17:32 +0900

    Add] 자료형이 아닌 템플릿 매개변수

---
+++ Chapter13/Chapter13_3/MyArray.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <assert.h> // assert
+#include <iostream>
+
+template<typename T, unsigned int T_SIZE> //m_length 대체
+class MyArray
+{
+private:
+	//int m_length;
+	T* m_data;  // T m_data[T_SIZE]
+
+public:
+	MyArray()
+	{
+		m_data = new T [T_SIZE];
+	}
+
+	~MyArray()
+	{
+		delete[] m_data;
+	}
+
+	T& operator[](int index) 	
+	{
+		assert(index >= 0 && index < m_length);
+		return m_data[index];
+	}
+
+	int getLength()
+	{
+		return T_SIZE;
+	}
+
+	void print();
+	{
+		for (int i = 0; i < T_SIZE; ++i)
+			std::cout << m_data[i] << " ";
+		std::cout << std::endl;
+	}
+};
+

28bccc5bc34f52b9e825a3ccd295870444052bbb
janjan97 <eoen482@nate.com>
2022-11-07 22:21:59 +0900

    Add] 예외처리의 위험성과 단점

---
+++ Chapter14/Chapter14_6/Chapter14_6.cpp
@@ -0,0 +1,44 @@
+﻿// Chapter14_6.cpp : 예외처리의 위험성과 단점
+#include <iostream>
+#include <memory> // unique_ptr
+using namespace std;
+
+/*
+int main()
+{
+	try
+	{
+		int* i = new int[1000000];  // 메모리 할당
+		//unique_ptr<int> up_i(i); // 스마트 포인터
+		throw "error";
+
+		delete[] i;
+	}
+	catch (...)
+	{
+		cout << "Catch" << endl;
+	}
+	return 0;
+}*/
+
+class A
+{
+public:
+	~A()
+	{
+		throw "error";  
+	}
+};
+
+int main()
+{
+	try
+	{
+		A a; 
+	}
+	catch (...)
+	{
+		cout << "Catch" << endl;
+	}
+	return 0;
+}

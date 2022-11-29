abd4e9234b84a2e9a707ea4957e6e5f9e19985cc
janjan97 <eoen482@nate.com>
2022-11-29 17:20:56 +0900

    Add] STL 반복자 (iterator)

---
+++ Chapter16/Chapter16_2/Chapter16_2.cpp
@@ -0,0 +1,38 @@
+﻿// Chapter16_2.cpp : STL 반복자 ( iterator )
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+using namespace std;
+
+int main()
+{
+	vector<int> container; //list, set, map
+	for (int i = 0; i < 10; ++i)
+		container.push_back(i);
+
+	vector<int>::iterator itr;
+	vector<int>::const_iterator citr;
+
+	// 1 - while문 사용
+	itr = container.begin();
+	while (itr != container.end())
+	{
+		cout << *itr << " ";
+
+		++ itr;
+	}
+	cout << endl;
+	// 2 - for문 사용
+	for (auto itr= container.begin(); itr != container.end(); ++itr)
+		cout << *itr << " ";
+	cout << endl;
+
+	// 3 - for each 문 사용
+	for (auto& e : container)
+		cout << e << " ";
+	cout << endl;
+
+	return 0;
+}

76b6358f948106fcd57371da6295c06b1c9df7d9
janjan97 <eoen482@nate.com>
2022-11-30 22:25:11 +0900

    Add] STL algorithm 소개

---
+++ Chapter16/Chapter16_3/Chapter16_3.cpp
@@ -0,0 +1,40 @@
+﻿// Chapter16_3.cpp :  STL algorithm
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> container;
+    for (int i = 0; i < 10; ++i)
+        container.push_back(i);
+
+    auto itr = min_element(container.begin(), container.end());
+    cout << *itr << endl;
+
+    cout << endl;
+
+    // find
+    itr = find(container.begin(), container.end(), 3);
+    container.insert(itr, 128);
+
+    for (auto& e : container) cout << e << " ";
+    cout << endl;
+
+    // sort
+    sort(container.begin(), container.end());
+    //li.sort();
+
+    for (auto& e : container) cout << e << " ";
+    cout << endl;
+
+    // reverse
+    reverse(container.begin(), container.end());
+    //li.reverse();
+
+    for(auto & e : container) cout << e << " ";
+    cout << endl;
+
+    return 0;
+}
